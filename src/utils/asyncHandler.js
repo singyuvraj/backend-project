da0c224ecf1f69502e61e724b3390dc76ada8fc0
singyuvraj <singyuvraj46@gmail.com>
2024-09-05 23:31:25 +0530

    Add refreshAccessToken endpoint

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,18 +4,24 @@ const asyncHandler = (requestHandler) => {
   }
 }
 
-export {asyncHandler}
+
+export { asyncHandler }
 
 
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
 
 
 // const asyncHandler = (fn) => async (req, res, next) => {
-//   try {
-//     await fn(req, res, next)
-//   } catch (error) {
-//     res.status(error.code || 500).json({
-//       success: false,
-//       message: error.message
-//     })
-//   }
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
 // }

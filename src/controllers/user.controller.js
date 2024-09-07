c8ba7401c31e1285a9d52a81a5c6818c414fae89
singyuvraj <singyuvraj46@gmail.com>
2024-09-07 13:17:03 +0530

    Mongodb Aggregation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -352,6 +352,81 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 })
 
+//Mongodb Aggregation function
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const {username} = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Please provide a username");
+  }
+
+const channel = await User.aggregate([
+  {
+    $match: {
+      username: username?.toLowerCase()
+    }
+  },
+  {
+    $lookup: {
+      from: "subscriptions",
+      localField: "_id",
+      foreignField: "channel",
+      as: "subscribers"
+    }
+  },
+  {
+    $lookup: {
+      from: "subscriptions",
+      localField: "_id",
+      foreignField: "subscriber",
+      as: "subscribedTo"
+    }
+  },
+  {
+    $addFields: {
+      subscribersCount: {
+        $size: "$subscribers"
+      },
+      channelsSubscribedToCount: {
+        $size: "$subscribedTo"
+      },
+      isSubscribed: {
+        $cond: {
+          if: {
+            $in: [
+              req.user?._id,
+              "$subscribers.subscriber"]},
+              then: true,
+              else: false
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      fullname: 1,
+      email: 1,
+      username: 1,
+      avatar: 1,
+      coverImage: 1,
+      subscribersCount: 1,
+      channelsSubscribedToCount: 1,
+      isSubscribed: 1
+    }
+  }
+])
+
+if (!channel?.length) {
+  throw new ApiError(404, "Channel not found");
+}
+
+return res
+.status(200)
+.json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"))
+
+})
+
 export { 
   registerUser,
   loginUser,
@@ -361,5 +436,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile
  }
